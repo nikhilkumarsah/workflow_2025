47731343982499081aff733bbd3faad28949af24
Nikhil_Kumar_sah <sahnikhilkr@gmail.com>
2025-06-18 23:06:32 +0530

    Initial commit

---
+++ Basic.js
@@ -0,0 +1,12 @@
+let a1 = "Jonny baba";
+let a2 = 'Jonny is "nostalgic"';
+let a3 = `Jonny has it's own way of talking`;
+
+console.log(a1+"/br",a2,a3);
+
+var str = " Nikhil ";
+console.log(str.length); // 6
+console.log(str.split("i"));
+console.log(str.trim());
+
+

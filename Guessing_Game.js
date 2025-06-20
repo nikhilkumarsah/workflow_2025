7d2342586e847706e02679bb1856022c36e25b3f
Nikhil_Kumar_sah <sahnikhilkr@gmail.com>
2025-06-20 11:46:05 +0530

    Initial commit

---
+++ Guessing_Game.js
@@ -0,0 +1,15 @@
+
+// let a = prompt("Enter the number to guess: ");
+
+// let b = Math.floor(Math.random() * 11 + 1);
+
+// while(a!=b){
+//     if(a>b){
+//         a = prompt("Your guess is too high. Try again!");
+//     }
+//     else if(a<b){
+//         a = prompt("Your guess is too low. Try again!");
+//     }
+    
+// }
+// alert("Congratulations! You guessed the number: " + b);
